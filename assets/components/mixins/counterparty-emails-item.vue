9482002cec84be8b3439053ef75a740f7ddd0acb
Maksimov Anton <anton25031995@gmail.com>
2019-06-23 23:14:52 +0300

    add show persons emails

--- assets/components/mixins/counterparty-emails-item.vue
+++ assets/components/mixins/counterparty-emails-item.vue
@@ -15,6 +15,28 @@
                 ></checking-email>
             </div>
         </div>
+        <div class="counterparty-emails-item__item-block" v-if="data.contactPersons.length">
+            <p class="counterparty-emails-item__item-title">Контактные лица:</p>
+            <div class="counterparty-emails-item__item-list">
+                <div class="counterparty-emails-item__person" v-for="person in data.contactPersons" :key="person.id">
+                    <p class="counterparty-emails-item__person-id">{{ person.id }}</p>
+                    <div class="counterparty-emails-item__person-header">
+                        <p class="counterparty-emails-item__person-full-name">
+                            {{ person.lastName }} {{ person.name }} {{ person.patronymic }}
+                        </p>
+                        <p class="counterparty-emails-item__person-position" v-if="person.position">{{ person.position }}</p>
+                    </div>
+                    <div class="counterparty-emails-item__person-emails">
+                        <checking-email v-for="email in person.emails"
+                                        :key="email.id"
+                                        :data="email"
+                                        :selected-emails="selectedEmails"
+                                        :set-email="setEmail"
+                        ></checking-email>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
